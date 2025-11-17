f6f8f958a9e8d7a99232b8aaf2fdeb6377ad89a3
Erbol Zhaparov <ae.270718@gmail.com>
2025-11-17 13:26:10 +0600

    add Response class, handled response in Server from handleResponse method

--- server/src/Response.java
+++ server/src/Response.java
@@ -2,39 +2,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class Server {
-    private final int port;
+public class Response {
+    private Response() {}
 
-    private final ExecutorService pool = Executors.newCachedThreadPool();
-
-    private Server(int port) {
-        this.port = port;
-    }
-
-    public static Server bindToServer(int port) {
-        return new Server(port);
-    }
-
-    public void run() {
-        try (ServerSocket server = new ServerSocket(port)) {
-            while(!server.isClosed()) {
-                Socket socket = server.accept();
-                pool.submit(() -> handle(socket));
-            }
-        } catch (IOException ioe) {
-            System.out.printf("Could not listen on port: %s", port);
-            ioe.printStackTrace();
-        }
-    }
-
-    private void handle(Socket clientSocket) {
+    public static void handleResponse(Socket clientSocket) {
         System.out.printf("Connected client: %s%n", clientSocket);
 
         try (
@@ -55,13 +30,20 @@ public class Server {
             ioe.printStackTrace();
         }
         System.out.println("Client disconnected: " + clientSocket);
+
     }
 
-    private PrintWriter getWriter(Socket clientSocket) throws IOException {
+    private static void sendResponse(String response, Writer writer) throws IOException {
+        writer.write(response);
+        writer.write(System.lineSeparator());
+        writer.flush();
+    }
+
+    private static PrintWriter getWriter(Socket clientSocket) throws IOException {
         return new PrintWriter(clientSocket.getOutputStream());
     }
 
-    private Scanner getReader(Socket clientSocket) throws IOException {
+    private static Scanner getReader(Socket clientSocket) throws IOException {
         return new Scanner(new InputStreamReader(clientSocket.getInputStream()));
     }
 
@@ -71,11 +53,5 @@ public class Server {
 
     private static boolean isEmptyMessage(String msg) {
         return msg == null || msg.isBlank();
-    }
-
-    private static void sendResponse(String response, Writer writer) throws IOException {
-        writer.write(response);
-        writer.write(System.lineSeparator());
-        writer.flush();
     }
 }
---
+++ server/src/Server.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int port;
+
+    private final ExecutorService pool = Executors.newCachedThreadPool();
+
+    private Server(int port) {
+        this.port = port;
+    }
+
+    public static Server bindToServer(int port) {
+        return new Server(port);
+    }
+
+    public void run() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            while(!server.isClosed()) {
+                Socket socket = server.accept();
+                pool.submit(() -> Response.handleResponse(socket));
+            }
+        } catch (IOException ioe) {
+            System.out.printf("Could not listen on port: %s", port);
+            ioe.printStackTrace();
+        }
+    }
+}
